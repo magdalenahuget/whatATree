5df33e5c27d9a9307494c1d86dbf6e8de38dc991
magdalenahuget <magdalena.huget@gmail.com>
2023-01-05 15:04:41 +0100

    Initial commit.

---
+++ client/public/script.js
@@ -0,0 +1,17 @@
+import { plants } from '/data.js';
+
+
+const divElement = function (content) {
+
+  return `<div>${content.name}</div>`;
+}
+
+const loadEvent = function () {
+
+  // Write your JavaScript code after this line
+
+  // Write your JavaScript code before this line
+
+}
+
+window.addEventListener("load", loadEvent);
